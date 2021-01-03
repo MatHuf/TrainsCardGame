51d8741a268a2c3e3f6d19bfec67f4e24a7441a3
MatHuf <53545249+MatHuf@users.noreply.github.com>
2021-01-03 14:29:55 -0500

    Add summary to score list
    Add rules
    Clean up styles and code

--- web/js/game.js
+++ web/js/game.js
@@ -1,9 +1,9 @@
 // Deck generation
-const suits = ["Hearts","Diamonds","Spades","Clubs"];
+const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
 const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
 const getNumberName = (numberValue) => {
-  switch(numberValue){
+  switch (numberValue) {
     case 1:
       return "One";
     case 2:
@@ -23,16 +23,16 @@ const getNumberName = (numberValue) => {
     case 9:
       return "Nine";
     case 10:
-        return "Ten";
+      return "Ten";
     default:
-        return "Unknown";
+      return "Unknown";
   }
-}
+};
 
 const getCard = (suit, rank) => {
   let value;
   let name;
-  switch(rank.toLowerCase()){
+  switch (rank.toLowerCase()) {
     case "a":
       value = 1;
       name = "Ace";
@@ -54,7 +54,7 @@ const getCard = (suit, rank) => {
       name = getNumberName(value);
   }
   return {
-    value, 
+    value,
     suit,
     rank,
     name,
@@ -62,20 +62,33 @@ const getCard = (suit, rank) => {
   };
 };
 
+const getSuitEmoji = (card) => {
+  switch (card.suit.toLowerCase()) {
+    case "hearts":
+      return "♥️";
+    case "diamonds":
+      return "♦️";
+    case "spades":
+      return "♠️";
+    case "clubs":
+      return "♣️";
+  }
+};
+
 const getDeck = () => {
   let cards = [];
-  for (let r = 0; r < ranks.length; r++){
-    for (let s = 0; s < suits.length; s++){
+  for (let r = 0; r < ranks.length; r++) {
+    for (let s = 0; s < suits.length; s++) {
       cards.push(getCard(suits[s], ranks[r]));
     }
   }
   return cards;
-}
+};
 
 // https://medium.com/swlh/the-javascript-shuffle-62660df19a5d
 function stackShuffle(deck) {
   let count = deck.length;
-  while(count) {
+  while (count) {
     deck.push(deck.splice(Math.floor(Math.random() * count), 1)[0]);
     count -= 1;
   }
@@ -86,7 +99,7 @@ function riffleShuffle(deck) {
   const leftHalf = deck.splice(0, cutDeckVariant);
   let leftCount = leftHalf.length;
   let rightCount = deck.length - Math.floor(Math.random() * 4);
-  while(leftCount > 0) {
+  while (leftCount > 0) {
     const takeAmount = Math.floor(Math.random() * 4);
     deck.splice(rightCount, 0, ...leftHalf.splice(leftCount, takeAmount));
     leftCount -= takeAmount;
@@ -95,25 +108,39 @@ function riffleShuffle(deck) {
   deck.splice(rightCount, 0, ...leftHalf);
 }
 
-// Prepare deck
-let deck = getDeck();
-stackShuffle(deck);
-riffleShuffle(deck);
-stackShuffle(deck);
-riffleShuffle(deck);
-stackShuffle(deck);
-riffleShuffle(deck);
-
+let deck = [];
 let train = [];
 let canPlay = true;
 let highScores = [];
+
+const prepareDeck = () => {
+  deck = getDeck();
+  stackShuffle(deck);
+  riffleShuffle(deck);
+  stackShuffle(deck);
+  riffleShuffle(deck);
+  stackShuffle(deck);
+  riffleShuffle(deck);
+};
+prepareDeck();
+
+// Add listener to game over input
+const nameInput = document.getElementById("new-score-name");
+nameInput.addEventListener("keydown", (e) => handleEnter(e));
+
+// Event handlers
+const handleEnter = (e) => {
+  if (e.key === "Enter") {
+    resetGame();
+  }
+};
 
 // Game actions
 const drawCard = () => {
   if (!canPlay) return;
   let newCard = deck.shift();
   // First card is always valid
-  let isValid = train.length > 0 
+  let isValid = train.length > 0
     ? isValidToPlay(train[train.length - 1], newCard)
     : true;
   if (!isValid) {
@@ -122,27 +149,29 @@ const drawCard = () => {
   }
   train.push(newCard);
   updateTrain(isValid);
-}
+};
 
 const resetGame = () => {
   recordScore();
   clearTrain();
+  prepareDeck();
   hideGameOver();
   canPlay = true;
-}
+};
 
 const resetScores = () => {
   highScores = [];
   updateScores();
-}
+};
 
 // Game rules
 const isValidToPlay = (currentCard, newCard) => {
-  // TODO handle aces being both high and low
+  // Potential upgrade: customize rules?
   if (currentCard.suit === newCard.suit) return true;
   if (currentCard.value <= newCard.value) return true;
+  if (newCard.rank.toLowerCase() === "a") return true;
   return false;
-}
+};
 
 // Display train
 const updateTrain = (isValid) => {
@@ -150,35 +179,21 @@ const updateTrain = (isValid) => {
   let parent = document.getElementById("train-container");
   let newCard = createCard(train[train.length - 1], isValid);
   if (!isValid) {
-    newCard.classList.add("invalid-card")
+    newCard.classList.add("invalid-card");
   }
   parent.appendChild(newCard);
-}
+};
 
 const clearTrain = () => {
   train = [];
   let trainCards = document.getElementById("train-container");
   trainCards.innerHTML = "";
-}
+};
 
 const createCard = (card, isValid) => {
   const cardContainer = document.createElement("div");
-  cardContainer.className = "card-container";
-  let suitChar;
-  switch (card.suit.toLowerCase()){
-    case "hearts":
-      suitChar = "♥";
-      break;
-    case "diamonds":
-      suitChar = "♦";
-      break;
-    case "spades":
-      suitChar = "♠";
-      break;
-    case "clubs":
-      suitChar = "♣";
-      break;
-  }
+  cardContainer.classList.add("card-container", "card-size");
+  const suitChar = getSuitEmoji(card);
   const content = document.createTextNode(`${card.rank} ${suitChar}`);
   cardContainer.appendChild(content);
   if (!isValid) {
@@ -187,7 +202,7 @@ const createCard = (card, isValid) => {
     cardContainer.appendChild(invalidIndicator);
   }
   return cardContainer;
-}
+};
 
 // Game over state
 const HIDDEN = "hidden";
@@ -201,12 +216,13 @@ const showGameOver = () => {
   newScore.innerHTML = train.length;
   const overlay = document.getElementById("overlay");
   overlay.classList.remove(HIDDEN);
-}
+  nameInput.focus();
+};
 
 const hideGameOver = () => {
   const overlay = document.getElementById("overlay");
   overlay.classList.add(HIDDEN);
-}
+};
 
 // High scores
 const recordScore = () => {
@@ -217,21 +233,22 @@ const recordScore = () => {
     // and we need to subtract one to account for it
     score: train.length - 1,
     rank: undefined,
+    summary: getTrainSummary(),
   });
   updateScores();
-}
+};
 
 const rankScores = () => {
   let rank = 0;
-  highScores.sort((a,b) => b.score - a.score);
+  highScores.sort((a, b) => b.score - a.score);
   highScores.forEach((s, i) => {
-    if (i - 1 >= 0 && highScores[i - 1].score === s.score){
+    if (i - 1 >= 0 && highScores[i - 1].score === s.score) {
       s.rank = rank;
     } else {
       s.rank = ++rank;
     }
   });
-}
+};
 
 const updateScores = () => {
   rankScores();
@@ -241,7 +258,17 @@ const updateScores = () => {
   const header = createScoreHeader();
   scoresList.appendChild(header);
   highScores.forEach(s => scoresList.appendChild(createScoreEntry(s)));
-}
+};
+
+const getTrainSummary = () => {
+  return train.map((c, i) => {
+    let cardSummary = `${c.rank}${getSuitEmoji(c)}`;
+    if (i === train.length - 1) {
+      cardSummary = `&#128683;${cardSummary}`;
+    }
+    return cardSummary;
+  });
+};
 
 const createScoreHeader = () => {
   const header = createScoreEntry({
@@ -251,7 +278,7 @@ const createScoreHeader = () => {
   });
   header.classList.add("score-list-header");
   return header;
-}
+};
 
 const createScoreEntry = (scoreEntry) => {
   const listItem = document.createElement("li");
@@ -267,5 +294,22 @@ const createScoreEntry = (scoreEntry) => {
   const scoreContainer = document.createElement("div");
   scoreContainer.textContent = scoreEntry.score;
   container.appendChild(scoreContainer);
+  if (scoreEntry.summary) {
+    const summaryToolTip = document.createElement("span");
+    summaryToolTip.classList.add("tooltip-text");
+    summaryToolTip.appendChild(createSummaryList(scoreEntry.summary));
+    container.appendChild(summaryToolTip);
+  }
   return listItem;
-}
+};
+
+const createSummaryList = (summary) => {
+  const list = document.createElement("ul");
+  list.classList.add("list-style-type-none");
+  summary.forEach(c => {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = c;
+    list.appendChild(listItem);
+  });
+  return list;
+};
